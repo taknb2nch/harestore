154d0131afb92ba69671c7a50d5ed6d60b1a1d42
taknb2nch <taknb2nch@gmail.com>
2026-09-08 11:15:56 +0900

    メソッド、関数名の変更とGenerateIDのDI

--- global.go
+++ global.go
@@ -65,7 +65,7 @@ func RunInTransaction(ctx context.Context, f func(ctx context.Context) error) er
 func GetByID[T any, PT PEntity[T]](ctx context.Context, id string) (*T, error) {
 	opts := getGlobalOptions()
 
-	return NewClient[T, PT](defaultRawClient, opts...).GetByID(ctx, id)
+	return NewClient[T, PT](defaultRawClient, opts...).Get(ctx, id)
 }
 
 // Insert registers one entity.
@@ -100,7 +100,7 @@ func Delete[T any, PT PEntity[T]](ctx context.Context, entity *T) error {
 func GetMultiByID[T any, PT PEntity[T]](ctx context.Context, ids []string) ([]*T, error) {
 	opts := getGlobalOptions()
 
-	return NewClient[T, PT](defaultRawClient, opts...).GetMultiByID(ctx, ids)
+	return NewClient[T, PT](defaultRawClient, opts...).GetMulti(ctx, ids)
 }
 
 // InsertMulti inserts the specifing entities.
@@ -135,12 +135,12 @@ func DeleteMulti[T any, PT PEntity[T]](ctx context.Context, entities []*T) error
 func RunRawQuery[T any, PT PEntity[T]](ctx context.Context, q *datastore.Query) ([]*T, error) {
 	opts := getGlobalOptions()
 
-	return NewClient[T, PT](defaultRawClient, opts...).RunRawQuery(ctx, q)
+	return NewClient[T, PT](defaultRawClient, opts...).RunQuery(ctx, q)
 }
 
 // DeleteByRawQuery deletes entities retrieved by executing a query.
 func DeleteByRawQuery[T any, PT PEntity[T]](ctx context.Context, q *datastore.Query) error {
 	opts := getGlobalOptions()
 
-	return NewClient[T, PT](defaultRawClient, opts...).DeleteByRawQuery(ctx, q)
+	return NewClient[T, PT](defaultRawClient, opts...).DeleteByQuery(ctx, q)
 }
